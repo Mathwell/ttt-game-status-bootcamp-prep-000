f0fbe626e9d6a01395ce9085d30c5e913cf3be8f
Olena Ageyeva <eageeva@hotmail.com>
2017-09-26 23:12:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,3 +5,23 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?
+  false
+end
+
+def full?
+  false
+end
+
+def draw?
+  false
+end
+
+def over?
+  false
+end
+
+def winner
+  "X"
+end
