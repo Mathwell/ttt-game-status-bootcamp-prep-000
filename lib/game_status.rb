ddeb44f053a9128ffda58181e318ecd35329ff40
Olena Ageyeva <eageeva@hotmail.com>
2017-09-26 23:27:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,10 +7,11 @@ end
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
+  won=[]
   won=WIN_COMBINATIONS.select { |combination|
     board[combination[0]]==board[combination[1]] && board[combination[1]]==board[combination[2]] && board[combination[1]]!=" "
   }
-  if won.length=0
+  if won.length==0
     return nil
   else
     return won
